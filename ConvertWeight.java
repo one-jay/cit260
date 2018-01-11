febf2a1ac640a3b46f23f465f2154ff0c62101b4
Clayton T <onejay@outlook.com>
2018-01-11 16:29:30 -0700

    add convertWeight file

---
+++ ConvertWeight.java
@@ -0,0 +1,11 @@
+public class ConvertWeight {
+  /**
+  The program starts in the main function
+  @param args the command line arguments
+  */
+  public static void main(String[] args) {
+
+    // print out the welcome message
+    System.out.println("This program converts pounds into kilograms");
+  }
+}
