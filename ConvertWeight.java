53d4ebeeee4a4fec1bec87fcc0eaeb8095d99884
Clayton Thomas <ct@Claytons-Air.T-mobile.com>
2018-01-11 20:03:36 -0700

    built out rest of program to convert weight

--- ConvertWeight.java
+++ ConvertWeight.java
@@ -1,11 +1,32 @@
+import java.util.Scanner;
+
 public class ConvertWeight {
+  private final static double KILOGRAMS_PER_POUND = 0.453592;
   /**
   The program starts in the main function
   @param args the command line arguments
   */
   public static void main(String[] args) {
-
     // print out the welcome message
     System.out.println("This program converts pounds into kilograms");
+    // prompt to enter the users name
+    System.out.println("\nPlease enter name:");
+    // create an input file for hte console
+    Scanner inFile;
+    inFile = new Scanner(System.in);
+    // read the value of the next line typed in the console
+    String name = inFile.nextLine();
+    // prompt to enter the weight in pounds
+    System.out.println("\nEnter the weight in pounds:");
+    // get the value entered for the weight
+    double weightInPounds = inFile.nextDouble();
+    // convert the weight to kilograms
+    double weightInKilograms = weightInPounds * KILOGRAMS_PER_POUND;
+    // display weight in kilograms to the console
+    System.out.println("\n++++++++++++++++++");
+    System.out.println("\t" + name
+                      + ", The weight is " + weightInKilograms
+                      + " kilograms");
+    System.out.println("+++++++++++++++++++\n");
   }
 }
